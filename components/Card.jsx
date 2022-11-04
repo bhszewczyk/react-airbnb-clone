92e9b7cfff912b9ec04f4d4a28dffafd3ae3302e
Barbara Szewczyk <comamysza@gmail.com>
2022-11-04 18:34:36 +0100

    Creates Cards basing on data

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './card.css';
 
-export default function Card() {
+export default function Card(props) {
 	return (
 		<div className='card'>
 			<div className='card-cover'>
-				<img src='/images/katie-zaferes.png' alt='Katie Zaferes' />
+				<img src={props.img} alt='activity' />
 				<span className='tag'>sold out</span>
 			</div>
 			<div className='card-info'>
@@ -13,15 +13,15 @@ export default function Card() {
 					<div className='reviews-location'>
 						<img src='/images/star.png' alt='star' className='star' />
 						<p>
-							<span>5.0</span>
-							<span className='gray'> (6) • </span>
-							<span className='gray'>USA</span>
+							<span>{props.rating}</span>
+							<span className='gray'> ({props.reviewCount}) • </span>
+							<span className='gray'>{props.location}</span>
 						</p>
 					</div>
 				</div>
-				<div className='card-info-summary'>Life lessons with Katie Zaferes</div>
+				<div className='card-info-summary'>{props.title}</div>
 				<div className='card-info--pricing'>
-					<strong>From $136</strong> / person
+					<strong>From ${props.price}</strong> / person
 				</div>
 			</div>
 		</div>
--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import './App.css';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Card from '../components/Card';
+import data from '../data';
 
 function App() {
+	const cards = data.map((item) => {
+		return (
+			<Card
+				img={`public/images/${item.coverImg}`}
+				rating={item.stats.rating}
+				reviewCount={item.stats.reviewCount}
+				location={item.location}
+				title={item.title}
+				price={item.price}
+			/>
+		);
+	});
+
 	return (
 		<div className='App'>
 			<Navbar />
 			<Hero />
-			<div className='wrapper'>
-				<Card />
-			</div>
+			<div className='wrapper'>{cards}</div>
 		</div>
 	);
 }
